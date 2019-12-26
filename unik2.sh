709e22d9fd844f593658604d5d19b082b85f6639
5w891 <58629942+wh891@users.noreply.github.com>
2019-12-26 21:44:25 +0800

    Update unik2.sh

--- unik2.sh
+++ unik2.sh
@@ -7,11 +7,11 @@ while [ $ulang = "y" ];
 do
 echo "untuk username dan password sc 68 adalah username : solo password : Player    SCRIPT ini akan di update kembali lagi untuk dikasih git terbaru"
 figlet "5W WE" | lolcat
-echo "______________________by 5W WE" | lolcat
+echo "______________________by 5W" | lolcat
   echo "\33[31;1m(_= 1 -+] install red hawk"
   echo "\33[1;33m[+- 2 =_# install sc 68"
   echo "\33[32;1m(_= 5 -+] exit"
-  echo "(+)(+)꧁F4K9࿇script࿈꧂(+)(+)" | lolcat
+  echo "(+)꧁F4K9࿇script࿈꧂(+)" | lolcat
 read -p "masukan kontol : " pilih
 if [ $pilih = "1" ];
 then
